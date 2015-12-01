d1be3c1f92f39b85843514f2d5ecc2e46e6107e8
Guillaume Lager <guillaume.lager@kalray.eu>
2015-12-01 14:30:51 +0100

    Add state machine for separate display and SEND/receive

--- ubberButton.ino
+++ ubberButton.ino
@@ -3,24 +3,34 @@
 #include <SPI.h>
 #include <RH_NRF24.h>
 #include <RH_ASK.h>
+#include <Wire.h>
 
 #define MSG_OK "OK              "
+
+typedef enum {
+  WAITING,
+  SENDING,
+  RECEIVED,
+  FOROTHER,
+  SENT,
+} state_t; 
 
 const int buttonPinPause = 2;     // the number of the pushbutton pin
 const int buttonPinRepas = 3;
 const int buttonPinReset = A6;
 
-const int ledPin = A0;      // the number of the LED pin
+      // the number of the LED pin
 const int ledPinR= 9;
 const int DIPPin = A7;
+const int LEDExtIOAddr = 0x38;
 
 //LCD
 const int LCDRSPin = 7;
 const int LCDEPin = 6;
-const int LCDData0 = A1;
-const int LCDData1 = A2;
-const int LCDData2 = A3;
-const int LCDData3 = A4;
+const int LCDData0 = A0;
+const int LCDData1 = A1;
+const int LCDData2 = A2;
+const int LCDData3 = A3;
 
 // variables will change:
 int buttonPause = 0;         // variable for reading the pushbutton status
@@ -33,45 +43,53 @@ const int d433_rx = 4;
 const int d433_tx = 5;
 
 UbberFrame f;
-UbberFrame *f_res;
 RH_NRF24 nrf24;
 
 RH_ASK driver(2000, d433_rx, d433_tx, A6, false);
 
+state_t state = WAITING;
+state_t old_state = WAITING;
+
+const int me = UbberFrame::GUILLAUME_L;
+
+
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(LCDRSPin, LCDEPin, LCDData3, LCDData2, LCDData1, LCDData0);
 
-void setup() {  
 
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);     
-  pinMode(ledPinR, OUTPUT); 
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPinPause, INPUT);     
-  pinMode(buttonPinRepas, INPUT);
-  pinMode(DIPPin, INPUT);
+unsigned long time;
+unsigned long lastStateChangeTime = 0;
 
-
-  Serial.begin(9600);
-
-
-  if(!nrf24.init())
-    Serial.println("nrf init failed");
-  if(!nrf24.setChannel(1))
-    Serial.println("setChannel failed");
-  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
-    Serial.println("setRF failed");
-  if(!driver.init()) {
-    Serial.println("433 init failed");
-  }  
-
-  analogReference(EXTERNAL);
-  lcd.begin(16, 2);
-  setState("Begin");
-
+void changeState(state_t new_state)
+{
+  state = new_state;
+  lastStateChangeTime = millis();
 }
 
-int setState(const char * state)
+unsigned long timeSinceStateChange()
+{
+	return millis() - lastStateChangeTime;
+}
+
+static void SetExtIO(int bit_no, int val)
+{
+  static uint8_t IOval = -1;
+  
+  if(val) IOval |= 1 << bit_no;
+  else IOval &= ~(1 << bit_no);
+  
+  Wire.beginTransmission(LEDExtIOAddr); // transmit to device #4
+  Wire.write(IOval);              // sends one byte setState 
+  Wire.endTransmission();    // stop transmitting
+  
+}
+
+static void setLED(int val)
+{
+  SetExtIO(0, !val);
+}
+
+int setDisplayState(const char * state)
 {
   lcd.setCursor(0, 0);
   lcd.clear();
@@ -93,6 +111,41 @@ int setStatus(const char *s1, const char *s2)
     strcat(toto, (const char *)s2);
     setStatus(toto);    
 }
+
+void setup() {  
+
+  // initialize the LED pin as an output:
+  pinMode(ledPinR, OUTPUT); 
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPinPause, INPUT);     
+  pinMode(buttonPinRepas, INPUT);
+  pinMode(DIPPin, INPUT);
+
+
+  Serial.begin(9600);
+
+
+  if(!nrf24.init())
+    Serial.println("nrf init failed");
+  if(!nrf24.setChannel(1))
+    Serial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("setRF failed");
+  if(!driver.init()) {
+    Serial.println("433 init failed");
+  }  
+  Wire.begin(); // join i2c bus (address optional for master)
+  analogReference(EXTERNAL);
+  lcd.begin(16, 2);
+  setDisplayState("Begin");
+  changeState(WAITING);
+
+}
+
+
+
+
+
 
 int getDip()
 {
@@ -116,15 +169,32 @@ int getDip()
     return 4;
   default:
     return 0xff;
-
   }
+}
+
+static int send(UbberFrame & frame)
+{
+  nrf24.send(frame.frameToChar(), frame.getLength());
+  driver.send(frame.frameToChar(), frame.getLength());
+  driver.waitPacketSent();
+  nrf24.waitPacketSent();
+   
+  return 0;
 }
 
 void clear()
 {
   lcd.clear();
-  setState("Waiting");
+  if(f.getSourceID() != me && f.getType() != UbberFrame::ACQUITTEMENT)  {
+    f.setType(UbberFrame::ACQUITTEMENT);
+    f.setDestID(f.getSourceID());
+    f.setSourceID(me);
+    send(f);
+  }
+  setDisplayState("Waiting");
   digitalWrite(ledPinR, LOW);
+  setLED(0);
+  changeState(WAITING);
 }
 
 uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
@@ -135,6 +205,9 @@ int r433_received = 0;
 int count;
 
 
+static int ledblue = 0;
+static int ledred = 1;
+
 void loop(){
   // read the state of the pushbutton value:
   buttonPause = digitalRead(buttonPinPause);
@@ -143,17 +216,21 @@ void loop(){
   
   if(buttonReset > 256)
     clear();
-    
+            
+   //uint8_t addr = nrf24.spiReadRegister(0x3);
+   //Serial.println(addr);
+   static int toto = 0;
+   if(toto < 3 ) {
+     nrf24.printRegisters();
+     toto++;
+   }
  
-  if( buttonPause == HIGH || buttonRepas == HIGH) {
-    setState("Sending");
-    setStatus("...");
+   if( (buttonPause == HIGH || buttonRepas == HIGH) && state != SENDING) {
+	changeState(SENDING);
     Serial.println("Begin send");
     dip = getDip();   
 
-    digitalWrite(ledPin, HIGH);
-
-    f.setSourceID(UbberFrame::GUILLAUME_L); 
+    f.setSourceID(me); 
     if(buttonPause == HIGH)
       f.setType(UbberFrame::PAUSE);
     else if(buttonRepas == HIGH)
@@ -175,25 +252,18 @@ void loop(){
     Serial.print("Sending ");
     Serial.print(f.getLength(), DEC);
     Serial.print("...");
-    nrf24.send(f.frameToChar(), f.getLength());
-    driver.send(f.frameToChar(), f.getLength());
-    driver.waitPacketSent();
-    nrf24.waitPacketSent();
-
-    delay(1000);  
+    
+    send(f);
     Serial.println("DONE");
-    setStatus(MSG_OK);
-    digitalWrite(ledPin, LOW);
+    setLED(0);
   }
   
   nrf_received = nrf24.available();
   r433_received = driver.recv(buf, &len);
   len = sizeof(buf);
-  if(nrf_received || r433_received)
-  {
-    digitalWrite(ledPinR, HIGH);
-    setState("Receiving");
-    setStatus("...");
+  if(nrf_received || r433_received )
+  {  
+
     if(nrf_received) Serial.println("Receiving from NRF");
     if(r433_received) Serial.println("Receiving from 433");
     int dest;
@@ -205,18 +275,65 @@ void loop(){
         Serial.print(buf[i], HEX); Serial.print(" ");
       }
 
-      f_res = new UbberFrame(buf, len);
-      dest = f_res->getDestID();
-      if(dest == UbberFrame::GUILLAUME_L || dest == UbberFrame::ALL)
+      if(f.frameFromChar(buf, len)) {
+        Serial.println("Bad frame");
+      }
+      dest = f.getDestID();
+      if((dest == UbberFrame::GUILLAUME_L || dest == UbberFrame::ALL))
       {
         Serial.print("Get ");
-        Serial.println(f_res->getTypeString());
-      }     
-      setState(f_res->getSourceIDString());
-      setStatus(f_res->getTypeString());    
-    }
-  
-    delete f_res;
+        Serial.println(f.getTypeString());
+		changeState(RECEIVED);
+      } else {
+		  changeState(FOROTHER);
+	  }	  
+    }  
   }
+
+  if(state == SENDING && timeSinceStateChange() > 1000) {
+	  changeState(SENT);
+  } else if(state == RECEIVED) {
+	  unsigned long time = millis();	  
+	  static unsigned long lastime = time;
+	  if(time - lastime > 500) {
+		  lastime = time;
+		  ledblue = !ledblue;
+		  ledred = !ledred;
+
+		  setLED(ledblue);
+		  digitalWrite(ledPinR, ledred);
+	  }	  
+  }
+
+  if(old_state != state) { // UPDATE display
+	  if(state == WAITING) {
+		  lcd.clear();
+		  setDisplayState("Waiting");	
+	  }
+	  else if(state == SENDING) {
+		  setLED(1);
+		  setDisplayState("Sending");
+		  setStatus("...");
+	  }
+	  else if(state == SENT) {
+		  setStatus(MSG_OK);
+		  setLED(0);
+	  }
+	  else if(state == RECEIVED) {
+		  setDisplayState(f.getSourceIDString());
+		  setStatus(f.getTypeString());
+	      digitalWrite(ledPinR, ledred);
+		  setLED(ledblue);
+		  
+	  }
+	  else if(state == FOROTHER) {
+		  setDisplayState("others");
+		  setStatus("for other");
+	  }	  
+  }
+
+  // State switch
+  old_state = state;
+  
 }
 
